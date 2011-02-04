4f87904abc844306caf07901520266005aa47583
Josh Nimoy <jtnimoy@jtnimoymacbookpro.lan>
2011-02-03 18:48:29 -0800

    growing control app more

--- apps/3d/Control/src/testApp.cpp
+++ apps/3d/Control/src/testApp.cpp
@@ -1,49 +1,98 @@
 #include "testApp.h"
 
+#include <netdb.h>
+
 
 //--------------------------------------------------------------
 void testApp::setup(){
 	
-/*
+	/**
 
-poll URLs on a set increment
-download their text
-parse as json
-interpret commands and act accordingly
- 
- */
-	ofSetVerticalSync(true);
+		poll URLs on a set increment
+		download their text
+		parse as json
+		interpret commands and act accordingly
+	 
+	 */
+	
+	ofSetWindowTitle("Donk Control");
 	tcpClient.setVerbose(true);
 	lastConnectTime = 0;
+	font.loadFont("uni05_54.ttf", 6);
+	font.setLineHeight(8);
+	
+	http_header_mode = true;
+	http_expect_connected = false;
+	
+	if(!settings.loadFromFile("settings.json.txt")){
+		string msg = "FATAL ERROR: failed to load JSON settings file: data/settings.json.txt";
+		log(msg);
+		cout << msg << endl;
+		_exit(1);
+	}
+	
+	polling_delay = settings["polling_delay_time_in_seconds"].asDouble();
+
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-	
+		
 	if(tcpClient.isConnected()){
 		string rcv = tcpClient.receiveRaw();
-		cout << rcv;
-		
-	}else if(ofGetElapsedTimef() - lastConnectTime > 2 || lastConnectTime == 0){
-		
-		// not connected, but waited long enough,
-		// or it's the first time, so calling right away
-		
-		if(!tcpClient.setup("173.236.165.127", 80, true)){
-			cerr << "Error: failed to setup TCP client" << endl;
+		if(tcpClient.getNumReceivedBytes() > 0){
+			parseReceivedBytes(rcv);
+		}
+	}else{
+		//is the connection freshly closed?
+		if(http_expect_connected){
+			//web page is done loading now.
+			http_expect_connected = false;
+			parseJSON(http_data_buffer);
 		}
 		
-		if(!tcpClient.sendRaw("GET / HTTP 1.0\n\n")){
-			cerr << "Error: failed to send HTTP GET to web server" << endl;
-		}
 		
-		lastConnectTime = ofGetElapsedTimef();
+		//is it time to call the server again?
+		if(ofGetElapsedTimef() - lastConnectTime > polling_delay || lastConnectTime == 0){
+	
+			int server_port = settings["server_port"].asInt();
+
+			http_header_mode = true;//go back to waiting for header chunk
+			http_data_buffer.clear(); //reset data buffer
+			
+			// not connected, but waited long enough,
+			// or it's the first time, so calling right away
+			
+			string ip = getIpFromDomain(settings["server"].asString());
+			if(ip==""){
+				log("Domain resolution failed. Network may be down.");
+			}else{
+				if(!tcpClient.setup(ip, server_port, false)){
+					log("failed to setup TCP client");
+				}else if(!tcpClient.sendRaw(string("GET ") + settings["path_buzz"].asString() + string(" HTTP 1.0\n\n"))){
+					log("failed to send HTTP GET to web server");
+					tcpClient.close();
+				}else{
+					log("calling server");
+					http_expect_connected = true;
+				}
+			
+			}
+			
+			lastConnectTime = ofGetElapsedTimef();
+		}
 	}
+	
+	
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
+	ofBackground(0,0,0);
+	ofColor(255,255,255);
+	glPushMatrix();
+	font.drawString(console,0,6);
+	glPopMatrix();
 }
 
 //--------------------------------------------------------------
@@ -81,4 +130,72 @@ void testApp::mouseReleased(int x, int y, int button){
 void testApp::windowResized(int w, int h){
 
 }
+
+//--------------------------------------------------------------
+
+string testApp::getIpFromDomain(string name){
+	hostent *h = gethostbyname(name.c_str());
+	if(h==NULL){
+		return string("");
+	}
+	char ipstr[18];
+	sprintf(ipstr,"%i.%i.%i.%i",
+			(unsigned char)h->h_addr_list[0][0],
+			(unsigned char)h->h_addr_list[0][1],
+			(unsigned char)h->h_addr_list[0][2],
+			(unsigned char)h->h_addr_list[0][3]);
+	return string(ipstr);
+}
+
+//--------------------------------------------------------------
+
+void testApp::log(string s){
+	console += ofGetTimestampString();
+	console += " ";
+	console += s + '\n';
+	
+	//erase old stuff
+	string::iterator it = console.begin();
+	int count = 0;
+	for(;it!=console.end();it++){
+		if(*it=='\n')count++;
+	}
+	int lineCount = (ofGetHeight()-6) / 8;
+	int eraseCount = 0;
+	while(eraseCount < count-lineCount){
+		if(console[0]=='\n')eraseCount++;
+		console.erase(0,1);
+	}
+}
+
+//--------------------------------------------------------------
+
+void testApp::parseReceivedBytes(string inBytes){
+	if(http_header_mode){
+		int nCount = 0;//consecutive line feed counter
+		string::iterator it = inBytes.begin();
+		for(;it!=inBytes.end();it++){
+			if(http_header_mode){
+				//just waiting for it to be over
+				if(*it=='\n'||*it=='\r'){
+					nCount++;
+					if(nCount==4){
+						http_header_mode = false;//start collecting the data from here	
+					}
+				}else nCount=0;
+			}else{
+				http_data_buffer += *it;
+			}
+		}
+	}else http_data_buffer += inBytes;//otherwise it's quite simple.
+}
+
+//--------------------------------------------------------------
+
+void testApp::parseJSON(string &data){
+	//json.parse(data);
+	//json.print();
+	cout << data << endl;
+}
+
 
--- apps/3d/Control/src/testApp.h
+++ apps/3d/Control/src/testApp.h
@@ -6,6 +6,12 @@
 #include "ofxJSON.h"
 #include "ofxNetwork.h"
 
+
+/**
+ 
+	implementation of non-blocking JSON loading from URLs and parsed out into OSC calls.
+ 
+ */
 class testApp : public ofBaseApp{
 
 public:
@@ -21,9 +27,73 @@ public:
 	void mouseReleased(int x, int y, int button);
 	void windowResized(int w, int h);
 
+	/**
+		my JSON parser instance
+	 */
 	ofxJSON json;
+	
+	/**
+		another JSON instance just for the app settings
+	 */
+	ofxJSON settings;
+	
+	/**
+		my ofxNetwork instance
+	 */
 	ofxTCPClient tcpClient;
+	
+	/**
+		last time a server pull was done (in seconds)
+	 */
 	float lastConnectTime;
+	
+	/**
+		window console buffer
+	 */
+	string console;
+	
+	/**
+		font for console rendering
+	 */
+	ofTrueTypeFont font;
+
+	/**
+		resolves a domain name to an IP address
+	 */
+	string getIpFromDomain(string name);
+
+	/**
+		logs to window console
+	 */
+	void log(string s);
+
+	/**
+		manages the HTTP protocol (separates header chunk from data chunk)
+	 */
+	void parseReceivedBytes(string incoming);
+	
+	/**
+		whether or not the connection is waiting for header to finish
+	 */
+	bool http_header_mode;
+	
+	/**
+		catching the returned "web page"
+	 */
+	string http_data_buffer;
+	
+	/**
+		whether or not we expect the socket to be open
+	 */
+	bool http_expect_connected;
+	
+	/**
+		parse the JSON into commands in preparation for OSC broadcasting
+	 */
+	void parseJSON(string &data);
+	
+	double polling_delay;
+	
 };
 
 #endif
