46ee1e1591faac585e367e338966a9db7cc9b17a
base <base@base.home>
2011-03-09 23:40:10 +0100

    adding msafluid

--- apps/3d/Render/src/ParticleSystem.cpp
+++ apps/3d/Render/src/ParticleSystem.cpp
@@ -9,21 +9,57 @@
 
 #include "ParticleSystem.h"
 
-using namespace MSA;
+//using namespace MSA;
 
 ParticleSystem::ParticleSystem() 
 {
 	curIndex = 0;
-	setWindowSize( ofVec2f( 1, 1 ) );
+	//setWindowSize( ofVec2f( 1, 1 ) );
 	
 	shader.setup("pointShader.vs", "pointShader.fs" );
 	
 	ofDisableArbTex();
-	starImage.loadImage("dust2.png");
+	starImage.loadImage("pointSpriteImg.png");
 	ofEnableArbTex();
 	
 	drawingType = SHADED_POINT_SPRITE;	
 }
+
+void ParticleSystem::init()
+{
+	// setup fluid stuff
+	fluidSolver.setup(100, 100);
+    fluidSolver.enableRGB(true);
+	//fluidSolver.setFadeSpeed(0.000001);
+	fluidSolver.setFadeSpeed(0.002f);
+	//fluidSolver.setFadeSpeed(1.0);
+	fluidSolver.setDeltaT(1);
+	fluidSolver.setColorDiffusion(0);
+	fluidSolver.setVisc(0.00015);
+	fluidDrawer.setup( &fluidSolver );
+	
+	setWindowSize(ofVec2f(ofGetWidth(), ofGetHeight()));
+	
+	fluidCellsX			= 150;
+	
+	drawFluid			= true;
+	drawParticles		= true;
+	
+	pMouse = ofVec2f(ofGetWidth()/2, ofGetHeight()/2);
+	resizeFluid			= true;
+	
+	currentEmitter = 0;
+	float inc = ofGetWidth()/3.f;
+	for(int i = 0; i<NUM_EMITTERS/2; i++)
+	{
+		forceEmitters[i].set(i*inc, ofGetHeight());
+	}
+	
+	for(int i = NUM_EMITTERS/2; i<NUM_EMITTERS; i++)
+	{
+		forceEmitters[i].set((i-NUM_EMITTERS/2)*inc, ofGetHeight()/2);
+	}
+}	
 
 void ParticleSystem::setWindowSize( ofVec2f winSize )
 {
@@ -33,6 +69,37 @@ void ParticleSystem::setWindowSize( ofVec2f winSize )
 
 void ParticleSystem::updateAndDraw( bool drawingFluid ){
 
+	// manage
+	fluidSolver.update();
+	
+	for(int i = 0 ; i<NUM_EMITTERS; i++)
+	{
+		
+		ofVec2f vel(0,ofRandom(1.f) * -1);
+		
+		ofVec2f constrainPos(
+							 ofMap(forceEmitters[i].x, 0, 1024, 0.f, 1.f, true),
+							 ofMap(forceEmitters[i].y, 0, 768, 0.f, 1.f, true) 
+							 );
+		
+		const float colorMult = 100;
+		const float velocityMult = 0.3;
+		
+		//addToFluid(forceEmitters[i], vel, true, true);
+		int index = fluidSolver.getIndexForPos(constrainPos);
+		fluidSolver.addForceAtIndex(index, vel * velocityMult);
+	}
+	
+	if(ofGetFrameNum() % 2 == 0 ) {
+		
+		ofVec2f pos(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()));
+		ofVec2f vel(sin(ofGetFrameNum()), cos(ofGetFrameNum()));
+		
+		addToFluid(pos, vel, true, true);
+		
+	}
+	
+	// update each particle
 	
 	for(int i=0; i<MAX_PARTICLES - 1; i++) {
 		if(particles[i].alpha > 0) {
@@ -168,3 +235,37 @@ void ParticleSystem::addParticle( const ofVec2f &pos ) {
 		//return;
 	}
 }
+
+
+// add force and dye to fluid, and create particles
+void ParticleSystem::addToFluid( ofVec2f pos, ofVec2f vel, bool addColor, bool addForce ) {
+    //float speed = vel.x * vel.x  + vel.y * vel.y * getWindowAspectRatio() * getWindowAspectRatio();    
+	//balance the x and y components of speed with the screen aspect ratio
+	float aspectRatio = ofGetWidth()/ofGetHeight();
+	float speed = vel.x * vel.x  + vel.y * vel.y * aspectRatio * aspectRatio;
+    if(speed > 0) {
+		ofVec2f constrainPos(
+							 ofMap(pos.x, 0, 1024, 0.f, 1.f, true),
+							 ofMap(pos.y, 0, 768, 0.f, 1.f, true) 
+							 );
+		
+        const float colorMult = 100;
+        const float velocityMult = 30;
+		
+		int index = fluidSolver.getIndexForPos(constrainPos);
+		
+		if(addColor) {
+			ofColor drawColor;
+			drawColor.setHex(0x5E2612, 1.0);
+			//drawColor.setHsb((ofGetFrameNum() % 360 ) / 360.0f, 1, 1);
+			
+			fluidSolver.addColorAtIndex(index, drawColor);// * colorMult);
+		}
+		
+		
+		//particleSystem.addParticles( pos * ofVec2f( ofGetWindowSize() ), 5 );
+		addParticles( pos, 3 );
+		//fluidSolver.addForceAtIndex(index, vel * velocityMult);
+    }
+}
+
