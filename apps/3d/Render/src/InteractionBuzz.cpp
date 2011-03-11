68f23f0a19456c287e2078754e4bfa37d0146da9
csugrue <csugrue@gmail.com>
2011-03-11 06:51:19 +0100

    buzz interaction kill fix and pop ups

--- apps/3d/Render/src/InteractionBuzz.cpp
+++ apps/3d/Render/src/InteractionBuzz.cpp
@@ -35,27 +35,26 @@ void InteractionBuzz::newBubbleRecieved(Donk::BubbleData * data) {
 	int id = bubbles.size()-1;
 	ofVec3f momPos = bubbles[id]->rigidBody->getPosition();
 	
-	
-	int   total = 10;
+	// base size on how many we have (need real data here )
+	int   total = 4;
 	float cRad = CONTAINER_RADIUS;
 	float volCont = (( 4.0/3.0)*PI )*(cRad*cRad*cRad);
 	float volBubb = volCont / (total * 5.f);
 	
 	float radius = volBubb / (( 4.0/3.0)*PI );
 	radius = pow(radius, 1.0f/3.0f);
-	
 	float hRad = cRad *.5;
 	
+	// create inner content bubbles
 	for( int i = 0; i < total; i++)
 	{
-		
 		ofVec3f startPos = ofVec3f(momPos.x+ofRandom(-hRad,hRad),momPos.y+ofRandom(-hRad,hRad),momPos.z+ofRandom(-hRad,hRad));
-		
 		
 		ContentBubble * bubble = new ContentBubble();
 		
 		bubble->data	  = data;
 		bubble->radius    = radius;
+		bubble->originalRadius = radius;
 		
 		btTransform startTransform;
 		startTransform.setIdentity();
@@ -78,7 +77,9 @@ void InteractionBuzz::newBubbleRecieved(Donk::BubbleData * data) {
 		
 		bubbles.push_back(bubble);
 		
-		bubbleTypes.push_back(BUZZ_TYPE_BUBBLE_IN);
+		bubble->buzzID = BUZZ_TYPE_BUBBLE_IN;
+		
+		// record types and mom index
 		bubbleToContIndex.insert( pair<int,int>(bubbles.size()-1,id) );
 		
 	}
@@ -88,31 +89,54 @@ void InteractionBuzz::newBubbleRecieved(Donk::BubbleData * data) {
 //--------------------------------------------------------
 void InteractionBuzz::update() {
 	
-	bool bAllOffFadedOut = true;
-	bool bAllOffScreen = true;
+	bool bAllOffFadedOut	= true;
+	bool bAllOffScreen		= true;
 
 	map <int, int> :: const_iterator it;
 	
 	for(int i=0; i<bubbles.size(); i++) {
 		
 		if(nTouches == 0) {
-			bubbles[i]->bTouched = false;			
+			
+			bubbles[i]->bTouched = false;	
+			
+			
+			if(bubbles[i]->bDoubleTouched ){
+				bubbles[i]->setRadius(bubbles[i]->originalRadius);
+			}
+			
+			
+			bubbles[i]->bDoubleTouched = false;		
 		}
 		
+		//----- when not touched
 		if(!bubbles[i]->bTouched) {
 			
 			if(bAnimateIn) {
+				
 				if(bubbles[i]->alpha <= 255) bubbles[i]->alpha += 10;
-				if(bubbleTypes[i] != BUZZ_TYPE_BUBBLE_IN) bubbles[i]->gotoTarget();
+				
+				// inner bubbles do not seek targets
+				if(bubbles[i]->buzzID != BUZZ_TYPE_BUBBLE_IN) bubbles[i]->gotoTarget();
+				
+				// inner bubbles fall to bottom
+				if(bubbles[i]->buzzID == BUZZ_TYPE_BUBBLE_IN)
+				{
+					bubbles[i]->rigidBody->body->applyCentralForce(btVector3(0,40,0));
+				}
 			}
 			
 			else if(bAnimateOut && !bDoneAnimatingOut) {
 				
-				//if(bubbles[i]->alpha > 0) {
-					//bubbles[i]->alpha -= 10;
-					//bAllOffFadedOut = false;
-				//}
+				// fade out
+				if(bubbles[i]->alpha > 0) {
+					bubbles[i]->alpha -= 2;
+					bAllOffFadedOut = false;
+				}
+				
+				// move off screen
 				bubbles[i]->goOffScreen();
+				
 				if(bubbles[i]->getPosition().y > 0) {
 					bAllOffScreen = false;
 				}
@@ -121,57 +145,49 @@ void InteractionBuzz::update() {
 			
 		}
 		
-		// inner bubbles fall to bottom of container
-		if(bubbleTypes[i] == BUZZ_TYPE_BUBBLE_IN)
-		{
-			bubbles[i]->rigidBody->body->applyCentralForce(btVector3(0,40,0));
-		}
-		else if(bubbleTypes[i] == BUZZ_TYPE_CONTAINER)
+		
+		// always constrain container structure to rigid body bubbles
+		if(bubbles[i]->buzzID == BUZZ_TYPE_CONTAINER)
 		{
 			((BuzzContainerBubble*)bubbles[i])->updateConstraint();
-		}
-		else if(bubbleTypes[i] == BUZZ_TYPE_BUBBLE_OUT)
-		{
-			if(bubbles[i]->getPosition().y > 0) 
-				if(bubbles[i]->alpha > 0) bubbles[i]->alpha -= 10;
-				//bubbles[i]->alpha = 0;
 		}
 		
 		
 		bubbles[i]->update();
 		
+		
+		
 		testApp::instance->projection->champagne.particles.push_back( new BrownianObject( bubbles[i]->pos, 0));
 
 	}	
 	
-	if(bAllOffScreen && bAnimateOut) {
+	if(bAllOffFadedOut && bAnimateOut){
 		bDoneAnimatingOut = true;
 		for(int i=0; i<bubbles.size(); i++) {
 			bubbles[i]->rigidBody->body->setActivationState(DISABLE_SIMULATION);
 		}
 		
 	}
-	
 }
 
 //--------------------------------------------------------
 void InteractionBuzz::drawContent(){
 	
-	/*
+	
 	 for(int i=0; i<bubbles.size(); i++){
-	 if(bubbles[i]->bAlive){
-	 if(bubbleTypes[i] == BUZZ_TYPE_CONTAINER)
-	 {
-	 ((BuzzContainerBubble*)bubbles[i])->globe->drawChildren();
+		if(bubbles[i]->bAlive){
+		 if(bubbles[i]->buzzID == BUZZ_TYPE_CONTAINER)
+		 {
+			//((BuzzContainerBubble*)bubbles[i])->globe->drawChildren();
+		 }
+		}
 	 }
-	 }
-	 }
-	 */
+	 
 	
 	for(int i=0; i<bubbles.size(); i++) {
 		if(bubbles[i]->bAlive){
 			bubbles[i]->drawHighLight();
-			if(bubbleTypes[i] != BUZZ_TYPE_CONTAINER) bubbles[i]->drawTwitterData();
+			if(bubbles[i]->buzzID != BUZZ_TYPE_CONTAINER) bubbles[i]->drawTwitterData();
 		}
 	}
 	
@@ -205,6 +221,23 @@ void InteractionBuzz::animatedOut() {
 	
 	bDoneAnimatingOut = false;
 	bDoneAnimatingIn  = true;
+	
+	for(int i=0; i<bubbles.size(); i++) 
+	{
+		bubbles[i]->offScreenTaget = bubbles[i]->rigidBody->getPosition();
+		bubbles[i]->offScreenTaget.y = -300;
+	}
+	
+	/*for(int i=0; i<bubbles.size(); i++) 
+	{
+		if(bubbleTypes[i] == BUZZ_TYPE_CONTAINER && bubbles[i]->bAlive)
+		{
+			clearContainer(i);
+			releaseContainedBubbles(i);
+		}
+	}*/
+	
+
 }
 
 //--------------------------------------------------------
@@ -224,6 +257,24 @@ void InteractionBuzz::animatedIn() {
 		bubbles[i]->rigidBody->body->setActivationState(DISABLE_DEACTIVATION);
 	}
 	
+}
+
+void InteractionBuzz::killallBubbles() {
+	
+	for (int i=0; i<bubbles.size(); i++) {
+		if(bubbles[i]->buzzID == BUZZ_TYPE_CONTAINER)
+		{
+			((BuzzContainerBubble*)bubbles[i])->globe->destroy();
+			bullet->world->removeConstraint(((BuzzContainerBubble*)bubbles[i])->p2p);
+		}
+		bubbles[i]->rigidBody->destroy();
+		delete bubbles[i];
+		bubbles[i] = NULL;
+	}
+	bubbles.clear();
+	
+	// also clear types and map
+	bubbleToContIndex.clear();
 }
 
 void InteractionBuzz::createContainerBubble(Donk::BubbleData * data){
@@ -248,7 +299,7 @@ void InteractionBuzz::createContainerBubble(Donk::BubbleData * data){
 	container->rigidBody->shape = new btSphereShape(radius/SCALE);
 	container->rigidBody->createRigidBody(1, startTransform);
 	
-	container->target.set(center.x + ofRandom(-300, 300), ofRandom(500, interactiveRect.height-300), 0);
+	container->target.set(center.x + ofRandom(-300, 300), ofRandom(300, interactiveRect.height-300), 0);
 	container->targetForce = 5;
 	
 	container->offScreenTaget.x = container->target.x;
@@ -261,8 +312,15 @@ void InteractionBuzz::createContainerBubble(Donk::BubbleData * data){
 	bullet->world->addRigidBody(container->globe->body, COL_CONT_FRAME,contFrameCollidesWith);
 	bullet->rigidBodies.push_back(container->globe);
 	
+	container->buzzID = BUZZ_TYPE_CONTAINER;
 	bubbles.push_back(container);
-	bubbleTypes.push_back(BUZZ_TYPE_CONTAINER);
+}
+
+void InteractionBuzz::clearContainer( int index ){
+	BuzzContainerBubble* bubble = (BuzzContainerBubble*)bubbles[index];
+	bubble->pop();
+	setCollisionFilter(bubble->rigidBody,COL_NOTHING,collidesWithNone);
+	bullet->world->removeRigidBody(bubble->rigidBody->body);
 }
 
 void InteractionBuzz::doubleTouched(ofVec2f touchpos)
@@ -273,14 +331,9 @@ void InteractionBuzz::doubleTouched(ofVec2f touchpos)
 	
 	for(int i=0; i<bubbles.size(); i++)
 	{
-		if(bubbleTypes[i] == BUZZ_TYPE_CONTAINER && ((BuzzContainerBubble*)bubbles[i])->bTouched && bubbles[i]->bAlive)
+		if(bubbles[i]->buzzID == BUZZ_TYPE_CONTAINER && ((BuzzContainerBubble*)bubbles[i])->bTouched && bubbles[i]->bAlive)
 		{
 			poppedID = i;
-			
-			BuzzContainerBubble* bubble = (BuzzContainerBubble*)bubbles[i];
-			bubble->pop();
-			bullet->world->removeRigidBody(bubble->rigidBody->body);
-			
 			break;
 		}
 	}
@@ -288,24 +341,45 @@ void InteractionBuzz::doubleTouched(ofVec2f touchpos)
 	
 	if(poppedID > -1)
 	{
+		clearContainer(poppedID);
 		clearOldBubbles();
 		releaseContainedBubbles(poppedID);
 		
+	}else{
+	
+		
+		for(int i=0; i<bubbles.size(); i++) {
+			
+			if(bubbles[i]->buzzID != BUZZ_TYPE_BUBBLE_OUT) continue;
+			
+			ContentBubble * bubble = bubbles[i];
+			ofVec2f p1  = touchpos;
+			ofVec2f p2  = bubble->rigidBody->getPosition();
+			float	dis = p1.distance(p2);
+			
+			if(dis < bubble->radius + 10.0) {
+				bubble->setRadius(150);
+				bubble->doubleTouched();
+				printf("hit this bubble: %p\n", bubble);
+				break;
+			}
+		}
+	
 	}
-	
-	
 	
 }
 
 
 void InteractionBuzz::clearOldBubbles()
 {
+	cout << "CLEAR OLD " << endl;
 	for(int i=0; i<bubbles.size(); i++)
 	{
-		if(bubbleTypes[i] == BUZZ_TYPE_BUBBLE_OUT)
+		if(bubbles[i]->buzzID == BUZZ_TYPE_BUBBLE_OUT)
 		{
+			bubbles[i]->target.x = bubbles[i]->rigidBody->getPosition().x;
 			bubbles[i]->target.y = -300;//bubbles[i]->offScreenTaget;
-			bubbleTypes[i] == BUZZ_TYPE_BUBBLE_OLD;
+			bubbles[i]->buzzID = BUZZ_TYPE_BUBBLE_OLD;
 		}
 	}
 }
@@ -316,15 +390,15 @@ void InteractionBuzz::releaseContainedBubbles(int poppedID)
 	
 	for(int i=0; i<bubbles.size(); i++)
 	{
-		if(bubbleTypes[i] == BUZZ_TYPE_BUBBLE_IN)
+		ContentBubble * bubble = bubbles[i];
+		if(bubble->buzzID == BUZZ_TYPE_BUBBLE_IN)
 		{
-			ContentBubble * bubble = bubbles[i];
-			
+						
 			it =  bubbleToContIndex.find(i);
 			
 			if( it != bubbleToContIndex.end() && it->second == poppedID )
 			{
-				bubbleTypes[i] = BUZZ_TYPE_BUBBLE_OUT;
+				bubble->buzzID  = BUZZ_TYPE_BUBBLE_OUT;
 				bubble->target = bubble->rigidBody->getPosition();
 				bubble->target.x += ofRandom(-300, 300);
 				bubble->target.y += ofRandom(-300, 300);
