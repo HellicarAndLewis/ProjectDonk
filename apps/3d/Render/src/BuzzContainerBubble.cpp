ce9683a387e90982ee9f9bf1e9ac07cbcd2c3845
csugrue <csugrue@gmail.com>
2011-03-11 01:56:45 +0100

    buzz container as compound form

--- apps/3d/Render/src/BuzzContainerBubble.cpp
+++ apps/3d/Render/src/BuzzContainerBubble.cpp
@@ -14,10 +14,61 @@ BuzzContainerBubble::BuzzContainerBubble(){
 	ContentBubble::ContentBubble();
 }
 
-void BuzzContainerBubble::createContainerBubble(ofxBullet * bullet){
+void BuzzContainerBubble::createContainerBubble(ofxBullet * bullet, ofVec3f _pos){
 	
 	ContentBubble::createContentBubble();
 	
+	globe = new ofxBulletCompoundBody();
+	globe->world = bullet->world;
+	globe->compoundShape = new btCompoundShape();
+	
+	int size = 80;
+	
+	btTransform startTrans;
+	startTrans.setIdentity();
+	startTrans.setOrigin( btVector3(pos.x,pos.y,pos.z) );
+	
+	btVector3 btSize;
+	btSize.setX( 200.f );
+	btSize.setY( 1.f );//btSize.setY( 30.f );
+	btSize.setZ( 200.f );
+	
+	btVector3 up(0, 1, 0);
+	ofVec3f position = _pos;
+	
+	// create the sphere:
+	for (int i = 1; i < 12; i++){
+		for (int j = 0; j < 12; j++){
+			
+			btScalar cx = position.x + (cos(i) * sin(j) * btSize.getX() );
+			btScalar cy = position.y + (cos(j) * btSize.getX());
+			btScalar cz = position.z + (sin(i) * sin(j) * btSize.getX());
+			
+			btVector3 pos(cx, cy, cz);
+			btVector3 dist = pos - ofVec3fToBtVec(position);
+			dist.normalize();
+			
+			float theta = acos(dist.dot(up));
+			btVector3 cross = up.cross(dist);
+			cross.normalize();
+			btQuaternion quat(cross, theta);
+			
+			btTransform t;
+			t.setIdentity();
+			t.setOrigin(pos);
+			t.setRotation(quat);
+			
+			btCollisionShape * spherePart = new btBoxShape(btVector3(size, 1, size));
+			globe->compoundShape->addChildShape(t, spherePart);	
+		}
+	}
+	
+	globe->createCompoundBody(10, startTrans);
+	
+	// constraint to rigidBody
+	btVector3 localPivot = rigidBody->body->getCenterOfMassPosition();
+	p2p = new btPoint2PointConstraint(*globe->body, localPivot);
+	bullet->world->addConstraint(p2p);
 	
 }
 
@@ -25,12 +76,29 @@ void BuzzContainerBubble::pop(){
 	
 	if(bAlive)
 	{
-		cout << "POPPED" << endl;
 		bAlive = false;
+		
+		int nChildren = globe->compoundShape->getNumChildShapes();
+		
+		for( int i = nChildren-1; i >= 0; i--)
+		{
+			globe->compoundShape->removeChildShapeByIndex(i);
+		}
+		
 	}
 }
 
 
+void BuzzContainerBubble::updateConstraint() {
+	
+	
+	if(bAlive && rigidBody->isBody())
+	{
+		btVector3 localPivot = rigidBody->body->getCenterOfMassPosition();
+		p2p->setPivotB(localPivot);
+	}
+	
+}
 
 
 
